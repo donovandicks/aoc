4d835d76fcdf4135d477fdd738b1ae33b2d95325
Donovan Dicks <donovan.dicks@outlook.com>
2024-12-02 20:33:06 -0500

    D2P1 python

--- 2024/main.py
+++ 2024/main.py
@@ -1,11 +1,14 @@
 import argparse
-from typing import Callable, Counter
+import itertools
+from typing import Callable, Counter, TypeAlias
+
+Solution: TypeAlias = Callable[[str], int]
 
 
 def d1_get_lists(data: str) -> tuple[list[int], list[int]]:
     left, right = [], []
 
-    for line in data.split("\n"):
+    for line in data.splitlines():
         ln, rn = line.split("   ")
         left.append(int(ln))
         right.append(int(rn))
@@ -25,9 +28,32 @@ def d1p2(data: str) -> int:
     return sum(l * right_counts.get(l, 0) for l in left)
 
 
-registry: dict[str, Callable[[str], int]] = {
+def is_safe(line: list[int]) -> bool:
+    """Line is strictly ordered and all diffs are within [1, 3]"""
+    return ((sorted(line) == line) or (sorted(line, reverse=True) == line)) and all(
+        (1 <= abs(a - b) <= 3) for a, b in itertools.pairwise(line)
+    )
+
+
+def d2p1(data: str) -> int:
+    return len(
+        list(
+            filter(
+                is_safe,
+                [[int(i) for i in line.split(" ")] for line in data.splitlines()],
+            )
+        )
+    )
+
+
+def d2p2(data: str) -> int:
+    pass
+
+
+registry: dict[str, Solution] = {
     "d1p1": d1p1,
     "d1p2": d1p2,
+    "d2p1": d2p1,
 }
 
 
