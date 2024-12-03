64b370dd763af4ff43326b953a99e47a493f1e7c
Donovan Dicks <donovan.dicks@outlook.com>
2024-12-02 22:51:36 -0500

    Light refactor

--- 2024/bin/main.ml
+++ 2024/bin/main.ml
@@ -1,3 +1,7 @@
+let re = Str.regexp "   "
+
+let split_line (line: string): string list = Str.split re line
+
 let read_data (file: string) : string =
   let in_chan = open_in file in
   try
@@ -7,11 +11,6 @@ let read_data (file: string) : string =
   with e ->
     close_in_noerr in_chan; (* Still close channel in case error *)
     raise e
-
-let re = Str.regexp "   "
-
-let split_line (line: string): string list =
-  Str.split re line
 
 let d1_get_lists (data: string): int =
   let left, right = String.split_on_char '\n' data
@@ -23,15 +22,27 @@ let d1_get_lists (data: string): int =
       | _ -> failwith "incorrect sublist length")
     ([], []) in
 
-  let sorted_left = List.sort compare left in
   let sorted_right = List.sort compare right in
 
-  List.mapi
+  left
+  |> List.sort compare
+  |> List.mapi
     (fun i x -> (Int.abs(x - List.nth sorted_right i)))
-    sorted_left
   |> List.fold_left ( + ) 0
 
-let () = read_data "./inputs/d1p1.txt"
+let () = 
+  let day = ref 1 in
+  let part = ref 1 in
+
+  let speclist = [
+    ("--day", Arg.Set_int day, "AoC Day");
+    ("--part", Arg.Set_int part, "AoC Part");
+  ] in
+
+  Arg.parse speclist (fun _ -> ()) "Usage: aoc [--day day] [--part part]";
+
+  Printf.sprintf "./inputs/d%dp%d.txt" !day !part
+  |> read_data
   |> d1_get_lists
   |> string_of_int
   |> print_endline
